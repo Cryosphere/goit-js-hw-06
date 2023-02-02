b4bab8fa4115b89a1ce222d56cd2cef4fdb88415
Sergey Shmatok <trycifer@gmail.com>
2023-02-03 01:30:09 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+const ingredients =
+ [
+  "potatoes",
+  "mushrooms",
+  "garlic",
+  "tomatos",
+  "herbs",
+  "condiments",
 ];
+
+// Створення списку елементів
+const listItems = ingredients.map((ingredient) => {
+  const listItem = document.createElement('li');
+  listItem.textContent = ingredient;
+  listItem.classList.add('item');
+  return listItem;
+});
+
+// Додавання елементів до списку
+const list = document.querySelector('#ingredients');
+list.append(...listItems);
