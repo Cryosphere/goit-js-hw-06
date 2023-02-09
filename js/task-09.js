0d98a64dbe619c2a2247aca21389d7aeaf8bd6c9
Sergey Shmatok <trycifer@gmail.com>
2023-02-09 13:42:02 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -2,17 +2,14 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-// const changeColor = () => {
-//   const color = getRandomHexColor();
-//   document.body.setAttribute('style', `${color}`);
-// }
-
-const btnChageColor = document.querySelector('.change-color');
-btnChageColor.addEventListener('click', () => {
+const changeColor = () => {
   const color = getRandomHexColor();
-  document.body.setAttribute('style', `background:${color}`);
-  document.querySelector('.color').innerHTML = color;
-});
+   document.body.setAttribute('style', `background:${color}`);
+   document.querySelector('.color').innerHTML = color;
+}
+  
+const btnChageColor = document.querySelector('.change-color');
+btnChageColor.addEventListener('click', changeColor);
 
 
 
