e9e6d6074a977171bd90e93ed6c84797727ce69d
Sergey Shmatok <trycifer@gmail.com>
2023-02-09 12:15:46 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+    console.log(event.currentTarget.elements);
+  const {elements:{ email, password }} = event.currentTarget;
+    console.log(elements);
+  if (email.value === "" || password.value === "") {
+    alert();
+  }
+
+  console.log(`Login: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
