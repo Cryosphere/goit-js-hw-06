b69195ff8e26da377992255649cabce6eec9299a
Sergey Shmatok <trycifer@gmail.com>
2023-02-09 08:44:18 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -2,10 +2,7 @@ const inputElem = document.querySelector('#validation-input');
   inputElem.addEventListener('blur', () => {
       if (inputElem.value.length <= inputElem.getAttribute("data-length")) {
           inputElem.className = 'validation-input valid';
-          
-
       } else {
           inputElem.className = 'validation-input invalid';
       }
-      console.log(inputElem.getAttribute("data-length"));
   });
