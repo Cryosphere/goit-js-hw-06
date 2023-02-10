4f342de5180b7d263d333afb39af6eb9ba9b3bc3
Sergey Shmatok <trycifer@gmail.com>
2023-02-10 16:00:42 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -2,9 +2,21 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
-function createBoxes(amount) { let startSize = 30; for (let i = 0; i < amount; i++) { let box = document.createElement("div"); box.style.width = startSize + "px"; box.style.height = startSize + "px"; box.style.backgroundColor = getRandomHexColor(); document.getElementById("boxes").appendChild(box); startSize += 10; } }
+function createBoxes(amount) {
+  let startSize = 30; for (let i = 0; i < amount; i++) {
+    let box = document.createElement("div");
+    box.style.width = startSize + "px";
+    box.style.height = startSize + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    document.getElementById("boxes").appendChild(box);
+    startSize += 10;
+  }
+}
 
-function destroyBoxes() { let boxes = document.getElementById("boxes"); while (boxes.firstChild) { boxes.removeChild(boxes.firstChild); } }
+function destroyBoxes() {
+  let boxes = document.getElementById("boxes");
+  while (boxes.firstChild) { boxes.removeChild(boxes.firstChild); }
+}
 
 
 
